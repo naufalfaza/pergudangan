802c28139415c12fce8b7a2a4f70cb2f9b8c3bba
naufalfaza <naufalfaza410502@gmail.com>
2022-07-18 16:05:52 +0700

    Pergudangan WEB

---
+++ head.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pergudangan</title>
+</head>
+<!-- Bootstrap -->
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<!-- FontAwesome V5 (KIT BY NAUFAL FAZA FADHILAH) INI NGAMBIL DARI ONLINE-->
+<script src="https://kit.fontawesome.com/54dc7623fb.js" crossorigin="anonymous"></script>
+
+
+<body>
+<div class="container-fluid">
+	<nav class="navbar navbar-expand-lg bg-light">
+	  <div class="container-fluid">
+	    <a class="navbar-brand" href="#"><img src="img/warehouse.png"> Pergudangan</a>
+	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	      <span class="navbar-toggler-icon"></span>
+	    </button>
+	    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+	        <li class="nav-item">
+	          <a class="nav-link active" aria-current="page" href="index.php">Dashboard</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Barang</a>
+	        </li>
+	        <li class="nav-item dropdown">
+	          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+	            Gudang
+	          </a>
+	          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+	            <li><a class="dropdown-item" href="#">Pakaian</a></li>
+	            <li><a class="dropdown-item" href="#">Elektronik</a></li>
+	            <li><a class="dropdown-item" href="#">Makanan Atau Minuman</a></li>
+	            <li><a class="dropdown-item" href="#">Suku Cadang</a></li>
+	          </ul>
+	        </li>
+	        <li class="nav-item dropdown">
+	          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+	            Transaksi
+	          </a>
+	          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+	            <li><a class="dropdown-item" href="#">Pesanan</a></li>
+	            <li><a class="dropdown-item" href="#">Pengiriman</a></li>
+	            <li><a class="dropdown-item" href="#">Barang Reject</a></li>
+	            <li><a class="dropdown-item" href="#">Barang Diterima</a></li>
+	          </ul>
+	        </li>
+	      </ul>
+	      <form class="d-flex" role="search">
+	        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+	        <button class="btn btn-outline-success" type="submit">Search</button>
+	      </form>
+	    </div>
+	  </div>
+	</nav>
+</div>
---
+++ index.php
@@ -0,0 +1,66 @@
+<?php include 'head.php' ?>
+
+<div class="container-fluid">
+  <div class="form-group row">
+    <div class="col-md-12">
+      <hr>
+      <label class="col-form-label col-md-12"><h2><i class="fas fa-home"></i> Dasboard</h2></label>
+      <hr>
+    </div>
+    <div class="col-md-3">
+      <div class="card bg-secondary text-white">
+        <div class="card-header">
+          <label class="col-form-label col-md-12">Jumlah Barang Yang Masuk</label>
+        </div>
+        <div class="card-body">
+          <label class="col-form-label col-md-12"><h3>30</h3></label>
+        </div>
+        <div class="card-footer">
+          <a class="btn btn-outline-light btn-sm col-md-12"><i class="fas fa-arrow-right"></i> More Info</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="card bg-danger text-white">
+        <div class="card-header">
+          <label class="col-form-label col-md-12">Jumlah Barang Yang Di Kembalikan</label>
+        </div>
+        <div class="card-body">
+          <label class="col-form-label col-md-12"><h3>30</h3></label>
+        </div>
+        <div class="card-footer">
+          <a class="btn btn-outline-light btn-sm col-md-12"><i class="fas fa-arrow-right"></i> More Info</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="card bg-warning">
+        <div class="card-header">
+          <label class="col-form-label col-md-12">Jumlah Barang Dalam Pengiriman</label>
+        </div>
+        <div class="card-body">
+          <label class="col-form-label col-md-12"><h3>30</h3></label>
+        </div>
+        <div class="card-footer">
+          <a class="btn btn-outline-light btn-sm col-md-12"><i class="fas fa-arrow-right"></i> More Info</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="card bg-success text-white">
+        <div class="card-header">
+          <label class="col-form-label col-md-12">Jumlah Barang Dalam Pengiriman</label>
+        </div>
+        <div class="card-body">
+          <label class="col-form-label col-md-12"><h3>30</h3></label>
+        </div>
+        <div class="card-footer">
+          <a class="btn btn-outline-light btn-sm col-md-12"><i class="fas fa-arrow-right"></i> More Info</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include 'foot.php' ?>
+
