911cb2f7dcf1bea1e0a6baae956f364a77df1fc5
naufalfaza <naufalfaza410502@gmail.com>
2022-07-18 16:12:19 +0700

    Pergudangan WEB

--- head.php
+++ head.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Pergudangan</title>
+	<title>GudangKu</title>
 </head>
 <!-- Bootstrap -->
 <!-- CSS only -->
@@ -14,7 +14,7 @@
 <div class="container-fluid">
 	<nav class="navbar navbar-expand-lg bg-light">
 	  <div class="container-fluid">
-	    <a class="navbar-brand" href="#"><img src="img/warehouse.png"> Pergudangan</a>
+	    <a class="navbar-brand" href="#"><img src="img/warehouse.png"> GudangKu</a>
 	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	      <span class="navbar-toggler-icon"></span>
 	    </button>
