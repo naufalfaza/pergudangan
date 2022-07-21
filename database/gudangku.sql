f8d0b8e3796785b6e5a4360b5e00bf81dc0ceb17
naufalfaza <naufalfaza410502@gmail.com>
2022-07-21 23:28:06 +0700

    Pergudangan WEB Update Naufal

--- database/gudangku.sql
+++ database/gudangku.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 19, 2022 at 09:30 AM
+-- Generation Time: Jul 21, 2022 at 06:20 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -34,16 +34,15 @@ CREATE TABLE IF NOT EXISTS `barang` (
   `id_gudang` int(11) NOT NULL,
   `harga` bigint(20) NOT NULL,
   `status` enum('P','Y','N') NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `barang`
 --
 
 INSERT INTO `barang` (`id`, `nama`, `kategori`, `qty`, `id_gudang`, `harga`, `status`) VALUES
-(1, 'Laptop Asus', 'Elektronik', 1, 1, 1000000, 'Y'),
-(2, 'Smith', 'elektronik', 1, 1, 1, 'Y'),
-(3, 'Bloods', 'elektronik', 2, 1, 1000, 'Y');
+(1, 'Screamous', 'elektronik', 10, 1, 100000, 'Y'),
+(2, 'Smith', 'elektronik', 2, 1, 1000000, 'Y');
 
 -- --------------------------------------------------------
 
@@ -54,9 +53,33 @@ INSERT INTO `barang` (`id`, `nama`, `kategori`, `qty`, `id_gudang`, `harga`, `st
 CREATE TABLE IF NOT EXISTS `gudang` (
 `id` int(11) NOT NULL,
   `nama` varchar(100) NOT NULL,
+  `kategori` varchar(50) NOT NULL,
   `longitude` text NOT NULL,
   `latitude` text NOT NULL,
   `status` enum('P','Y','N') NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `gudang`
+--
+
+INSERT INTO `gudang` (`id`, `nama`, `kategori`, `longitude`, `latitude`, `status`) VALUES
+(1, 'Gudang Pajajaran', 'elektronik', ' 107.608407', '-6.900115', 'Y'),
+(2, 'Gudang 1', 'baju', ' 107.622379', '-6.946944', 'N'),
+(3, 'Gudang Percetakan', 'elektronik', ' 107.632619', '-6.925541', 'N');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE IF NOT EXISTS `transaksi` (
+`id` int(11) NOT NULL,
+  `id barang` int(11) NOT NULL,
+  `status` enum('Y','N') NOT NULL,
+  `tgl_record` date NOT NULL,
+  `jam_record` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -76,6 +99,12 @@ ALTER TABLE `gudang`
  ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+ ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -83,11 +112,16 @@ ALTER TABLE `gudang`
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `gudang`
 --
 ALTER TABLE `gudang`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
