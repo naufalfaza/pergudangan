41c7c3f1fede839508e4307b3110e0753172d910
naufalfaza <naufalfaza410502@gmail.com>
2022-07-21 21:22:23 +0700

    Pergudangan WEB Update Naufal

--- struktur/foot.php
+++ struktur/foot.php
@@ -2,29 +2,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 <!-- Leaflet -->
 <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js" integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ==" crossorigin=""></script>
-<script type="text/javascript">
-    // Maps Dashboard
-    //  Latitude, Longitude
 
-    var map = L.map('gudang_maps').setView([-6.9053933, 107.5771494], 13);
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-    
-    //  Icon
-    var gudangIcon = L.icon({
-        iconUrl: '../img/gudang.png',
-        iconSize:     [32, 32], // size of the icon
-        popupAnchor:  [0, -15] // point from which the popup should open relative to the iconAnchor
-    });
-    
-    <?php 
-        foreach ($db->data_gudang() as $result) {
-    ?>
-    L.marker([<?php echo $result['latitude']; ?>, <?php echo $result['longitude']; ?>], {icon: gudangIcon}).addTo(map).bindPopup('<b><?php echo $result['nama'] ." ".$result['kategori']; ?></b>');
-    <?php } ?>
-</script>
 <script type="text/javascript">
     // Maps Tambah Gudang
     var map = L.map('tambahgudang_maps').setView([-6.171505880059564, 106.82650155849288], 10);
@@ -50,6 +28,30 @@
         document.getElementById("latitude").value = lat[1];
         L.marker([lat[1], lng[0]], {icon: gudangIcon}).addTo(map).bindPopup('<b>Gudang</b>');
     });
+</script>
+
+<script type="text/javascript">
+    // Maps Dashboard
+    //  Latitude, Longitude
+
+    var map = L.map('gudang_maps').setView([-6.9053933, 107.5771494], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    
+    //  Icon
+    var gudangIcon = L.icon({
+        iconUrl: '../img/gudang.png',
+        iconSize:     [32, 32], // size of the icon
+        popupAnchor:  [0, -15] // point from which the popup should open relative to the iconAnchor
+    });
+    
+    <?php 
+        foreach ($db->data_gudang() as $result) {
+    ?>
+    L.marker([<?php echo $result['latitude']; ?>, <?php echo $result['longitude']; ?>], {icon: gudangIcon}).addTo(map).bindPopup('<b><?php echo $result['nama'] ." ".$result['kategori']; ?></b>');
+    <?php } ?>
 </script>
 </body>
 </html>
